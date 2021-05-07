1e48d738249e4e261b99198bcec1aa3aa0fe3b7b
Gord-Nik <75978452+Gord-Nik@users.noreply.github.com>
2021-05-07 09:18:52 +0300

    add changed

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,18 +10,16 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
-        logger.info("method login was called");
+        logger.debug("login method was called. Params: login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
-            logger.error("Password validation failed. Params: login = {}", login);
+            throw new AuthenticationException("Username or password are incorrect!Login:" + login);
         }
         return user;
     }
 
     private User findByLogin(String login) {
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
     }
