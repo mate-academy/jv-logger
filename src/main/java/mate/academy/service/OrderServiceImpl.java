f34f2d8a6378240e71573c7e9161ee6c11d6ab50
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-05-11 10:38:39 +0300

    fixed some style issues

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("Method completeOrder was called. "
+        logger.debug("Method completeOrder was called. "
                 + "With the following parameters: userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
@@ -25,8 +25,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Method getAllProductsFromShoppingCart was called with the following "
-                + "parameters: userId = {}", userId + ". And successfully fetched data from DB");
+        logger.info("Data was successfully fetched from DB with the following "
+                + "parameters: userId = {}", userId + ".");
         return products;
     }
 }
