475f51e48d585024d1496967e39806e91d5f3d14
Vlad-Andr <andreev.v99@gmail.com>
2021-05-11 10:42:34 +0300

    changed levels

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -13,7 +13,7 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Order completeOrder(Long userId) {
         // TODO: add log message about method completeOrder was called
-        logger.info("method completeOrder was called. By user with Id: {}", userId);
+        logger.debug("method completeOrder was called. By user with Id: {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -26,7 +26,7 @@ public class OrderServiceImpl implements OrderService {
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
         // TODO: add log message about successful fetched data from DB
-        logger.info("data was successful fetched from DB");
+        logger.debug("data was successful fetched from DB");
         return products;
     }
 }
