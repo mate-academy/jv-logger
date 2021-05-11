22dabe62b43ff616b43b5e0f9a7af8cca00f6c93
Maksim <max.freedman.main@gmail.com>
2021-05-11 11:04:58 +0300

    changed loggin level to DEBUG

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("Method completeOrder was called. "
+        logger.debug("Method completeOrder was called. "
                 + "Params: userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
@@ -21,13 +21,11 @@ public class OrderServiceImpl implements OrderService {
     }
 
     private List<Product> getAllProductsFromShoppingCart(Long userId) {
-        // NOTE: In production ready code this method should fetch data from DB
-        // For test purpose we simplify this method and return dummy data
         Product iphone = new Product("iPhone X", BigDecimal.valueOf(1199));
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Method getAllProductsFromShoppingCart was called. Params: userId = {}",
+        logger.debug("Method getAllProductsFromShoppingCart was called. Params: userId = {}",
                 userId);
         return products;
     }
