cb02caad46aa3875c1352dec86c60f24f2b18f4b
Gord-Nik <75978452+Gord-Nik@users.noreply.github.com>
2021-05-11 13:57:22 +0300

    change to OrderServiceImpl.java for logger lvl

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Fetched data from DB successful. UserId = {}",userId);
+        logger.info("Fetched data from DB successful. UserId = {}", userId);
         return products;
     }
 }
