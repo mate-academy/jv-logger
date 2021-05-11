919cc5bd65cdb4101195a26126039824050a38f6
ritakisluhina <marharita.kislukhina@gmail.com>
2021-05-11 16:13:47 +0200

    fixed

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("method completeOrder was called");
+        logger.debug("completeOrder method was called. Param = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("completeOrder method was called. Param = {}", userId);
+        logger.info("method getAllProductsFromShoppingCart was called");
         return products;
     }
 }
