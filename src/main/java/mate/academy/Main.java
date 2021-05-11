52cabe36e9708832f44dec3d30c2d351c3d012db
Дмитро <dima4994@gmail.com>
2021-05-11 12:27:45 +0300

    fixed all bugs notice by Kseniia Makarova!

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("We can't found user!", e);
+            logger.error("We can't find this user!", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
