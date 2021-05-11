e20a1f4c99ddaaa1be652014b5dbeeccb0c46a2c
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-05-11 16:01:01 +0300

    forth commit: deleted TODO comments

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,6 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        // TODO: add log message about method completeOrder was called
         logger.debug("\"completeOrder\" method was called. Input parameter: userId = {}",
                 userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
@@ -30,7 +29,6 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        // TODO: add log message about successful fetched data from DB
         logger.debug("List of products was successfully retrieved from DB");
         return products;
     }
