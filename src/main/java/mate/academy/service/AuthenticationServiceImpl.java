b01c4de47d8ca0999a47c46c58e00a23eeb974cd
Bohdan Chupika <boroda4436@users.noreply.github.com>
2021-06-15 16:07:53 +0300

    test Travis CI is running ok

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -17,6 +17,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private User findByLogin(String login) {
         User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
+        int a = 0;
         user.setUserId(2L);
         return user;
     }
