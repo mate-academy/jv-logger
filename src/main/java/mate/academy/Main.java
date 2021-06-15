d3e134c917442bd87e658e7884eb465f8e8762d9
Максим <mbelmasg@gmail.com>
2021-06-15 11:44:11 +0300

    Done, I hope.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,14 +11,14 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        //BasicConfigurator.configure();
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Can't login", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
