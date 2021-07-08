033a4e92204c33d7b47b55a6b5699e3d7d3ba936
Vova Bilodon <vovabilodon@gmail.com>
2021-07-08 17:00:02 +0300

    changed logger messages

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Cant authenticate user with login = bob");
+            logger.error("Cant authenticate user. ", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.debug("completeOrder method was called");
+        logger.debug("completeOrder method was called with userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
