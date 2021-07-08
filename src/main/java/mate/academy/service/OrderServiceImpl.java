d21b36a6a47fb5976828fa39af91ebc01298de09
Nikitozin <nikitozin91@gmail.com>
2021-07-08 19:30:48 +0300

    info_added

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("Method completeOrder was called");
+        logger.debug("Method completeOrder was called bu userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data successful fetched from DB. UserId = {}", userId);
+        logger.debug("Data successful fetched from DB. UserId = {}", userId);
         return products;
     }
 }
