f89796ff556ad1dbaad176cb462b6c24ede7ce6e
EugeneSinica <eugenesinica@gmail.com>
2021-07-08 15:04:55 +0300

    changed logger level from INFO to DEBUG in OrderServiceImpl

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("Complete order method was called with userId: " + userId);
+        logger.debug("Complete order method was called with userId: " + userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Successful fetching data from DB, userId: " + userId);
+        logger.debug("Successful fetching data from DB, userId: " + userId);
         return products;
     }
 }
