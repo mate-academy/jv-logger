8dae12034673ef40f67447a61143eb831da9c194
EugeneSinica <eugenesinica@gmail.com>
2021-07-08 15:24:52 +0300

    little fix in login method

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("Authentication try with login: " + login);
+        logger.debug("Authentication try with login: " + login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
