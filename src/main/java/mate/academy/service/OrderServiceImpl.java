11350c335797f512abfaa0c45d9d7dc8b840cb8d
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-07-08 15:57:06 +0300

    changed logger-level

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("that data was successfully fetched from db. Params: userId = {}",
+        logger.info("that data was successfully fetched from db. Params: userId = {}",
                 userId);
         return products;
     }
