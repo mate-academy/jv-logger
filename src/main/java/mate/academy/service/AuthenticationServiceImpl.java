ec0f5554a627a2f978cb05c29449e94a2e649ec2
Optipist <vlad.baydak28@gmail.com>
2021-07-09 10:55:38 +0300

    Fixed some perfectionists checkstyle

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.debug("Called authentication with login: {}",login);
+        logger.debug("Called authentication with login: {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
