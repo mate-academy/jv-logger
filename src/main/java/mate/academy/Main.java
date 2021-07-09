6de237fb745d2829251a089938f9ff0615d0cbd5
vKutik <84785564+vKutik@users.noreply.github.com>
2021-07-09 08:51:01 +0300

    rename Param to Parameter

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Error authentication ");
+            logger.error("Error authentication");
             return;
         }
         OrderService orderService = new OrderServiceImpl();
