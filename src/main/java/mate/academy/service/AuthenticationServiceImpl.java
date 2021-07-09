db2757ccf9378245b91f87bb70083a174db837ec
Optipist <vlad.baydak28@gmail.com>
2021-07-09 10:32:27 +0300

    deleted todos

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
         logger.debug("Called authentication with login: {}",login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
