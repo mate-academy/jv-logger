995db124a7688c9e8faae3c8e5bc9c273c45680e
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-09-15 11:13:29 +0300

    added empty line to log4j2.xml, made message in main more informative

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login! ", e);
+            logger.error("Can't login! Wrong password or username.", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
