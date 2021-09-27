9eea6d8a142f85a5f455a1e126a2376bacca0442
Alexandr <netiazhevskyi@gmail.com>
2021-09-27 20:28:34 +0300

    minot fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("AuthenticationException was caught: " + e);
+            logger.error("Unable to login, " + e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
