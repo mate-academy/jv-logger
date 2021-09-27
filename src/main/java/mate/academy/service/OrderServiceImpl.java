e67971bd64bcb37fd8128eef2af015f6c2a68315
Yuliya Senchenko <usencenko70@gmail.com>
2021-09-27 19:05:48 +0300

    Created jv-logger

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Method getAllProductsFromShoppingCart was called. Params: userId={}", userId);
+        logger.info("Successful fetched data from DB for userId={}", userId);
         return products;
     }
 }
