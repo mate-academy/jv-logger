bd9f568f3849c90a58b5edcc77e15de5223a1cc0
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-09-28 17:10:34 +0300

    Rewrited exceptions message and corrected mistakes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Incorrect login or password: ", e);
+            logger.error("Failed to authenticate user. Incorrect login or password: ", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
