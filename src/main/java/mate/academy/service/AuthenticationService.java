d2d3cce7b91f82aa198f13314798333cc2b5edb7
Denys Smei <denys.smei@artcodevision.com>
2021-09-28 12:58:41 +0300

    Log been added

--- src/main/java/mate/academy/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package mate.academy.service;
-
-import mate.academy.exception.AuthenticationException;
-import mate.academy.model.User;
-
-public interface AuthenticationService {
-    User login(String login, String password) throws AuthenticationException;
-}
--- src/main/java/mate/academy/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.service;
-
-import mate.academy.model.Order;
-
-public interface OrderService {
-    Order completeOrder(Long userId);
-}
