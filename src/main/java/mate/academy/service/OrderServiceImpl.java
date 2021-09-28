45aecb95822f2d1023f96f8fa0be08f2181b132e
Vladyslav Gural <vladgural@gmail.com>
2021-09-28 11:33:57 +0300

    used stream API and added user ID to logfile

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,10 +28,9 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("successful fetched data from DB. Products are:");
-        for (Product product : products) {
-            logger.info(product);
-        }
+        logger.info("successful fetched data from DB. UserID={}", userId);
+        logger.info("Products are:");
+        products.forEach(logger::info);
         return products;
     }
 }
