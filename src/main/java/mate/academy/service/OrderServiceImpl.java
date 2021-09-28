d82da168b6aebb41aa460aa03e1e053adc1b106c
Vitalii Mykh <vitaliymykh@gmail.com>
2021-09-28 18:24:03 +0300

    create more informative logs

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("method completeOrder was called");
+        logger.info("method completeOrder was called. Params: userId {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from DB was fetched");
+        logger.info("Data from DB was fetched. Params: userId {}");
         return products;
     }
 }
