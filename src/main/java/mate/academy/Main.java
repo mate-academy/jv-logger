8974d5ea75243a7d8d146486cbfd0b1a2f674e5f
Sasha Hordenin <sg743148@gmail.com>
2021-09-28 10:47:17 +0300

    added more informative message to log exception

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Incorrect login and/or password.", e);
+            logger.error("Failed to authenticate. Incorrect login and/or password.", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
