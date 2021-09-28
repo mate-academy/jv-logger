4c740a368c15acb6e0e55d9ff62a71f24bacebfd
Gleb Kreshchuk <gleb.net@gmail.com>
2021-09-28 10:54:07 +0300

    jv-logger task completed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.warn("Can't login", e);
+            logger.warn("Can't login. Login or password are incorrect", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
