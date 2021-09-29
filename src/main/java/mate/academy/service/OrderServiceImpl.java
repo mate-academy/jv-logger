c0987b9b5cfd21c06aa7d89a9c41fc87207bab22
Vladyslav Gural <vladgural@gmail.com>
2021-09-29 13:08:49 +0300

    coupled two line in one

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,8 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("successful fetched data from DB. UserID={}", userId);
-        logger.info("Products are:");
+        logger.info("successful fetched data from DB. UserID={}. Products are:", userId);
         products.forEach(logger::info);
         return products;
     }
