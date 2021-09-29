78a23048a371dadda6387da87b649850c978e8a7
Andrii Tyndiuk <andriytundyk@gmail.com>
2021-09-29 11:00:52 +0300

    fixed after mentor review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Failed authentication user", e);
+            logger.error("Failed authentication user, wrong login or password", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
