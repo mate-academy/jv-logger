dd2df53e63c77214d867e2b49b4d308d402ed0d1
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-09-29 10:09:29 +0300

    fixed logger  messegae in AuthenticationServiceImpl class in login method

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info(" Method login was called. USer login: {}", login);
+        logger.info(" Method login was called. User login: {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
