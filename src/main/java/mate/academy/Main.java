6deed638980720c3f6563d3205d17de290a71c99
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-11-29 16:52:27 +0200

    Configure logger after mentor review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can`t call login method", e);
+            logger.error("Can`t login", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
