c588fd5c49a63d6970ea44df2289421200e72374
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-11-30 11:54:29 +0200

    Error message fixed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Authentication failed, login - {}", "bob", e);
+            logger.error("Authentication failed. Incorrect login or password", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
