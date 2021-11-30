321ccecf167d743f160f7d6a10e5ab844ed6f16a
Olha Tankevych <freakystyley85@gmail.com>
2021-11-30 10:57:25 +0200

    changed messages

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from the DB was successfully retrieved. Params: userId = {}, products = {}", userId, products);
+        logger.info("Data from the DB was successfully retrieved. Params: userId = {}, "
+                + "products = {}", userId, products);
         return products;
     }
 }
