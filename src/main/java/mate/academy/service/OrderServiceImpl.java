4f530a20bdc6b575327fb3969b2580409d1c5a4c
Volodimir <vova.stepanenko2020@gmail.com>
2021-11-30 10:55:52 +0200

    one message for log products in cart

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,10 +24,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        products.forEach(product -> {
-            logger.info("Data from DB product: {} from cart for user {}",
-                    product.getTitle(), userId);
-        });
+        logger.info("Successful fetched data from DB"
+            + "Data: userId {}, products: {} ", userId, products);
         return products;
     }
 }
