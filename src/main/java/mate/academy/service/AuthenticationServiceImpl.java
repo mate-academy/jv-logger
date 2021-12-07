ba19a8bfc44ff5d8038c383dbfee6522247f13b3
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2021-12-07 21:30:53 +0200

    Fixed log4j version; Added loggers for services

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,11 +2,18 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger LOGGER = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        LOGGER.info(String.format("start AuthenticationServiceImpl.login(%s, ****)", login));
+        if (login.length() <= 1) {
+            LOGGER.warn(String.format("strange login was used: '%s'", login));
+        }
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
@@ -17,6 +24,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private User findByLogin(String login) {
         User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
+        LOGGER.info("set user ID");
         user.setUserId(2L);
         return user;
     }
