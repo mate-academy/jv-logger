45e49902ecf8e21d343b6535be041b8117537a2a
Alexander <aleksandr_pogreb@ukr.net>
2022-01-13 17:48:59 +0200

    fixed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login", e);
+            logger.warn("Can't login", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
