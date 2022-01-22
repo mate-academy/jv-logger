75e0560f7a667e538d3a2229af29e0d130e8d10a
Maksym Pokras <mxpokras@gmail.com>
2022-01-22 20:31:27 +0200

    Removed all trash from pom.xml, log4j2.xml located in src/main/resources

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,10 +6,16 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static Logger logger = LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+        logger.info("Main.main starting...");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
+        logger.info("authenticationService created");
         User user;
         try {
             user = authenticationService.login("bob", "1234");
