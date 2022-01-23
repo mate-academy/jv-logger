79a0050c8fa535c01bafedd176038bd0a7320289
Maksym Pokras <mxpokras@gmail.com>
2022-01-23 13:34:01 +0200

    getAllProductsFromShoppingCart log message modified

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("getAllProductsFromShoppingCart({}) fetched from DB {} products",
+        logger.info("getAllProductsFromShoppingCart(userId: {}) fetched from DB {} products",
                 userId, products.size());
         if (logger.isDebugEnabled()) {
             logger.debug("getAllProductsFromShoppingCart returns products: {}",
