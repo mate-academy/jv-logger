729b63eed1735693fc107bbfadc5891ab6979749
AprioriKS <sounddummies@gmail.com>
2022-02-19 20:02:53 +0200

    fix main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can`t login", e);
+            logger.error("Failed to login. Params: login = {}", "bob", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
