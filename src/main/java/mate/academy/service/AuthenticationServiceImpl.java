45a93350a2afbf9be6fca2a16c3900d4198148e9
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-02-23 21:20:10 +0200

    JC-Logger. remove all comments. after student review

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -20,7 +20,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private User findByLogin(String login) {
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
     }
