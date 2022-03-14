38756e64cb83d4330889157f194c4afa027647aa
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-03-14 15:10:15 +0200

    fixed remarks

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,8 +10,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        final Logger logger = LogManager.getLogger(Main.class);
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
