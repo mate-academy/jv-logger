81b635511d7e3fb61a482b6c69646a65981a963f
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-03-19 20:14:20 +0200

    Logger was successfully implemented, some classes were edited

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,18 +6,39 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int USER_LOGIN_NUMBER = 3;
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user;
-        try {
-            user = authenticationService.login("bob", "1234");
-        } catch (AuthenticationException e) {
-            e.printStackTrace();
-            return;
-        }
+        logger.info("AuthenticationService object was created");
+        User user = null;
         OrderService orderService = new OrderServiceImpl();
-        orderService.completeOrder(user.getUserId());
+        logger.info("OrderService object was created");
+        for (int i = 0; i < USER_LOGIN_NUMBER; i++) {
+            try {
+                switch (i) {
+                    case 0:
+                        user = authenticationService.login("bob", "1234");
+                        break;
+                    case 1:
+                        user = authenticationService.login("jake", "1234");
+                        break;
+                    case 2:
+                        user = authenticationService.login("bob", "qwerty");
+                        break;
+                    default:
+                        break;
+                }
+            } catch (AuthenticationException e) {
+                logger.error("Can't login user", e);
+                return;
+            }
+            orderService.completeOrder(user.getUserId());
+        }
     }
 }
