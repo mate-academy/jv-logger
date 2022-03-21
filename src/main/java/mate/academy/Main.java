a3598a976decf494393d1e3ad171c812e4e0fae6
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-03-21 19:12:32 +0200

    Logger was successfully implemented, some classes were edited

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,34 +11,19 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
-    private static final int USER_LOGIN_NUMBER = 3;
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        logger.info("AuthenticationService object was created");
-        User user = null;
+        User user;
+        User invalidUser;
         OrderService orderService = new OrderServiceImpl();
-        logger.info("OrderService object was created");
-        for (int i = 0; i < USER_LOGIN_NUMBER; i++) {
-            try {
-                switch (i) {
-                    case 0:
-                        user = authenticationService.login("bob", "1234");
-                        break;
-                    case 1:
-                        user = authenticationService.login("jake", "1234");
-                        break;
-                    case 2:
-                        user = authenticationService.login("bob", "qwerty");
-                        break;
-                    default:
-                        break;
-                }
-            } catch (AuthenticationException e) {
-                logger.error("Can't login user", e);
-                return;
-            }
+        try {
+            user = authenticationService.login("bob", "1234");
             orderService.completeOrder(user.getUserId());
+            invalidUser = authenticationService.login("bob", "qwerty");
+            orderService.completeOrder(invalidUser.getUserId());
+        } catch (AuthenticationException e) {
+            logger.error("Can't login user", e);
         }
     }
 }
