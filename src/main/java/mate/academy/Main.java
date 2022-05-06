c9e81de10daf93766e82c57380c0983df06357b5
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-05-06 17:13:30 +0300

    fix mistake

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.debug("Can't find similar login", e);
+            logger.error("Can't find similar login", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
