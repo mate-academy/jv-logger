ef8d64257e16cead701fab8ddccbae96c1d90092
mordast7 <mordast7@gmail.com>
2022-05-07 22:32:58 +0300

    fixed mistakes

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,8 +12,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("Method completeOrder was called. " +
-                "Parameters: userId = {}", userId);
+        logger.info("Method completeOrder was called. "
+                + "Parameters: userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
