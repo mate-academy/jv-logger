15e5ef68b6e3feeac971f6a28312c0a552e7d38c
Oleksander <ghjwtyrj25@gmail.com>
2022-05-09 17:45:11 +0300

    After changes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login.", e);
+            logger.error("User authentication was failed!", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
