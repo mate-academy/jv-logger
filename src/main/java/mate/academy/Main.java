c35dcb15fa07faeed6c98e04c216a033bd791ce8
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-05-09 15:35:40 +0300

    logger homework solution was fixed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can not login", e);
+            logger.error("User authentication was failed. Username or password are incorrect", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
