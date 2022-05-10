cce02eb8869f0dade57c9294150bd74e8695102c
mordast7 <mordast7@gmail.com>
2022-05-10 11:49:43 +0300

    moved log4j2.xml to directory marked as Resources Root change log level in OrderServiceImpl from debug to info

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -25,7 +25,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("Data was successfully fetched from DB. Parameters: userId = {}", userId);
+        logger.info("Data was successfully fetched from DB. Parameters: userId = {}", userId);
         return products;
     }
 }
