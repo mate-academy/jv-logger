4ca5724ec740af77b970e450a4c0b4db14891e43
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-24 02:54:42 +0300

    Initial solution commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,7 @@
 package mate.academy;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -8,16 +10,21 @@ import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.trace("Main jv-logger started");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Login failed", e);
             return;
         }
+        logger.info("Login of user={} was successful", user.getLogin());
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
+        logger.trace("Main jv-logger stopped");
     }
 }
