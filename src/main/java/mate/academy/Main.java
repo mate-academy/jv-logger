3b954c0f1f4a355a124a263136fb8f23f7cafda0
kqlynero <kqlynero@gmail.com>
2022-07-04 11:45:51 +0300

    -fixed mistakes after review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login user. Params: user={}" + user.getLogin(), e );
+            logger.error("Can't login user. Params: user={}" + user.getLogin(), e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
