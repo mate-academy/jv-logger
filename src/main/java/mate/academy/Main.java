1bad061b5690662779bdb5cbe687b0c52b019985
Vladimir Chuvashov <vova5179@gmail.com>
2022-07-04 20:23:42 +0300

    fixed mistakes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,8 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login. Login or password"
-                    + System.lineSeparator() + " are incorrect", e);
+            logger.error("Can't login. Login or password are incorrect", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
