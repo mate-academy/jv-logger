f63445ed2e461873197d63d182ba83fcaaac86b5
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-08-08 21:26:56 +0200

    fixed code bugs #1

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -6,13 +6,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("Method login() was called.");
+        logger.info("Method 'login' was called. Login: {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
+            logger.error("Authentication error!");
             throw new AuthenticationException("Username or password are incorrect");
         }
         return user;
