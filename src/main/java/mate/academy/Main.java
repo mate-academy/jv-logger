fefb1ba7acfbaca193b7eef12141dcb7c064e6c8
AndriiP <andrey.pslk@gmail.com>
2022-08-26 19:09:47 +0300

    How I can replace completeOrder method invoke from catch block and to not initialize user(withou compile error) ?

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -14,13 +14,13 @@ public class Main {
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user = null;
+        User user;
         try {
             user = authenticationService.login("bob", "1234");
+            OrderService orderService = new OrderServiceImpl();
+            orderService.completeOrder(user.getUserId());
         } catch (AuthenticationException e) {
             logger.error("Can't login", e);
         }
-        OrderService orderService = new OrderServiceImpl();
-        orderService.completeOrder(user.getUserId());
     }
 }
