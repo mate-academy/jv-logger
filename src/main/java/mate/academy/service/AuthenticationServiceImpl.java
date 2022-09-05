53a254c16c2a34dbcf8c9e56d3c49e1872ab49d3
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-05 19:42:57 +0300

    code style

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -7,9 +7,10 @@ import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("login method was called. Params: login = {}" , login);
+        logger.info("Method login was called. Params: login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
