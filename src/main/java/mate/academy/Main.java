ba59bb875637a447bce0694bb731ec2b5ab759bd
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-05 19:40:56 +0300

    return to good password

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -16,7 +16,7 @@ public class Main {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "10234");
+            user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
             logger.error("Authentication failed. Wrong credentials.", e);
             return;
