fc8c2f8583460c7a7f34e546b594040e43fc661e
GolDenik <denik.golibroda@gmail.com>
2022-09-05 14:56:20 +0300

    after mvn clean package

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Products were fetched from DataBase SUCCESSFULLY. Params: userId = {}", userId);
+        logger.info("Products were fetched from DataBase SUCCESSFULLY. "
+                + "Params: userId = {}", userId);
         return products;
     }
 }
