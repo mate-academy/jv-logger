c2b1962e70f2829f990a0eabe21ba4835c743b62
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-05 15:36:57 +0200

    add improvments

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,14 +11,15 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user = null;
-
+        User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Try again! Invalid data! ", e);
+            logger.error("User can't login. Invalid data!", e);
+            return;
         }
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
