b38e247c212ba2750a99d50a7ac524eb90c02d41
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-09-05 16:14:57 +0300

    add products for log

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from DB was successfuly fetched");
+        logger.info("{} from DB was successfuly fetched", products);
         return products;
     }
 }
