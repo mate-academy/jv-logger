23e16fe8b3d578bf29d242b2d86e47f7e0c1cc8c
Thousandfold <you@example.com>
2022-09-06 11:29:37 +0300

    fix_checkstyle

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login with data",e);
+            logger.error("Can't login with data", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
