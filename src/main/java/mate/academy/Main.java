b1ad1203d1b2a19f430694fb398bb186362fd42b
asha <ashajanebrown@gmail.com>
2022-09-06 13:54:05 +0300

    fixed check style issues

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -9,9 +9,9 @@ import mate.academy.service.OrderServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
