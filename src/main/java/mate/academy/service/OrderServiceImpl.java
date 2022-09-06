9e67ce69fa408b475fa419be45ea03d323273f9c
Misha Brovkin <haasiakira@gmail.com>
2022-09-06 12:18:17 +0300

    improved code after student review

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("completeOrder method was called. Params: userId = {}", userId);
+        logger.info("Method completeOrder was called. Params: userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("method getAllProductsFromShoppingCart was called. "
+        logger.info("Method getAllProductsFromShoppingCart was called. "
                 + "Data was fetched successful. Params: userId = {}", userId);
         return products;
     }
