71e1bb1cf8e9ac9fef208a80c202a899ae39e32f
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-09-30 15:07:54 +0300

    help with logger

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -1,24 +1,24 @@
 package mate.academy.service;
 
-import mate.academy.Main;
-import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
-    public User login(String login, String password) throws AuthenticationException {
+    public User login(String login, String password) {
         logger.info("Method login was called. Params: login={}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
-            logger.error("Username or password are incorrect. Params: login={}", login);
+            logger.warn("Username or password are incorrect. Params: login={}", login);
         }
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.info("Method findByLogin was called. Params: login={}", login);
         User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
