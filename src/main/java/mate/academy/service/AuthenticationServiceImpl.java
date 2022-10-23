fe96c3720d0d51449c2910118c332e800c6088be
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-10-23 19:19:40 +0300

    Logger solution v1

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -19,7 +19,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     private User findByLogin(String login) {
-        User user = new User(login, "1232");
+        User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
