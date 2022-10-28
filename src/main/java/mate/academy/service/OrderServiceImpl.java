b1888dcf8128fbdb3e839acbccc13bb50efc99f9
Nazarii Pertsak <pertsak98@gmail.com>
2022-10-28 14:28:26 +0300

    implemented a bit optimization in log messages

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("Method completeOrder was called. Params: userId = " + userId);
+        logger.info("Method completeOrder was called. Params: userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,8 +24,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Method getAllProductsFromShoppingCart was called. Params: userId = "
-                + userId);
+        logger.info("Method getAllProductsFromShoppingCart was called. Params: userId = {}",
+                userId);
         return products;
     }
 }
