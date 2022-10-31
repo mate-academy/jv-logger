d8f3134d14e02f273829888e1cf6d017760b4844
Pavlo Gook <pavlogook@gmail.com>
2022-10-31 18:10:52 +0200

    Code refactoring

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    public static final String PASSWORD = "1234";
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
 
     @Override
@@ -19,7 +20,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     private User findByLogin(String login) {
-        User user = new User(login, "1234");
+        User user = new User(login, PASSWORD);
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from DB was fetched successfully");
+        logger.info("Data from DB was fetched successfully. Params: userId='{}'", userId);
         return products;
     }
 }
