31901f3f7bc90c02105a5189e3d8148d3b7ce458
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-11-01 11:26:33 +0200

    Changes after mento rewiev

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -19,7 +19,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Attempt to login was failed.", e);
+            logger.error("Attempt to login failed.", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
