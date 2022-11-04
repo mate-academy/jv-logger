813ff89e8756b48b4a7cc2d9dac470deda770b2f
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-11-04 06:08:35 +0200

    added fixes after review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,15 +10,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final String LOGIN = "bob";
+    private static final String PASSWORD = "1234";
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(LOGIN, PASSWORD);
         } catch (AuthenticationException e) {
-            logger.warn("Authentication failed", e);
+            logger.warn("Authentication failed with login " + LOGIN, e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
