91bc06e21321dd825c9f21adda6c8e3e65b425ea
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-11-08 15:29:06 +0200

    Solution for Logger Task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,15 +6,21 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final String DEFAULT_LOGIN = "bob";
+    private static final String DEFAULT_PASSWORD = "bob";
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(DEFAULT_LOGIN, DEFAULT_PASSWORD);
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Login error. Params: login = {}",DEFAULT_LOGIN);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
