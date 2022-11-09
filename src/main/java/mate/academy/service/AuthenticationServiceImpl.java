25add1e95e0e6ff32502e8a2db6b9ca66bf13f30
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-11-09 11:30:35 +0200

    Used logger.debug and logger.error where it is more appropriate

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("login() method was called, params: login='{}'", login);
+        logger.debug("login() method was called, params: login='{}'", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
