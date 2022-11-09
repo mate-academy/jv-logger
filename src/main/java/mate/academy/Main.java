80103dbe5ded63987b6c720be760cff135b1decf
yehbreus <yeh.breus@gmail.com>
2022-11-09 13:12:25 +0200

    change logger.info to logger.error in main class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.info("Can't authentication user", e);
+            logger.error("Can't authentication user", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
