c844d0a842f8f2b6dba35f623cb848d3bffc2c67
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-11-09 08:58:50 +0200

    Correcting the code; Added Logger;

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,15 +6,21 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    public static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+    private static final String LOGIN = "bob";
+    private static final char [] PASSWORD = { '1','2','3','4' };
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(LOGIN, String.valueOf(PASSWORD));
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Can't identification with login " + LOGIN, e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,20 +2,26 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    public static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+    private static final char [] PASSWORD = { '1','2','3','4' };
+    private static final String INCORRECT_DATA = "Username or password are incorrect";
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        logger.info("Login is: " + login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
-            throw new AuthenticationException("Username or password are incorrect");
+            throw new AuthenticationException(INCORRECT_DATA);
         }
         return user;
     }
 
     private User findByLogin(String login) {
-        User user = new User(login, "1234");
+        User user = new User(login, String.valueOf(PASSWORD));
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
