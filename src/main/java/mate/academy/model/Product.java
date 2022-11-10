f39edb1413b68b6f0a2bc83b45bde0a03a75509a
Danyl Kniaziev <kdanil0401@gmail.com>
2022-11-10 16:45:01 +0200

    added log products inside OrderServiceImpl

--- src/main/java/mate/academy/model/Product.java
+++ src/main/java/mate/academy/model/Product.java
@@ -1,8 +1,6 @@
 package mate.academy.model;
 
 import java.math.BigDecimal;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Product {
     private String title;
