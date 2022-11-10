12107c97e3c9028a5fc5bb9cba95165e6ed79348
thebitler <yurchyknikita@gmail.com>
2022-11-10 09:41:09 +0100

    Add func return data to logger

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        LOGGER.info("Data successfully fetched from DB, Param: userId={}", userId);
+        LOGGER.info("Data successfully fetched from DB, Param: userId={}, products={}", userId, products);
         return products;
     }
 }
