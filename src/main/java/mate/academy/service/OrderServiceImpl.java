54eed528b527c2a636cf42b830e36255698c5e98
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-11-10 14:46:54 +0200

    added products to log info

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from the DB is fetched. Params: userId = {}", userId);
+        logger.info("Data from the DB is fetched. Params: userId = {}, products={}",
+                userId, products);
         return products;
     }
 }
