7fbac37eb82434cf3524ae24c5a343cfce0ca99f
thebitler <yurchyknikita@gmail.com>
2022-11-10 11:55:58 +0100

    fix

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -20,6 +20,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private User findByLogin(String login) {
         User user = new User(login, "1234");
+        LOGGER.info("Method findByLogin was called. Params: login={}", login);
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
