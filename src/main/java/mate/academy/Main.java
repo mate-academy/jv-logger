3bf94c14120f564d5f80579e1947452d81c1a30f
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-25 20:31:22 +0200

    mvn fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         logger.info("Method 'main' was called. Program is started");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
