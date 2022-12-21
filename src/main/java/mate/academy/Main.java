61c103f095d46cb27d16f3fc5f2ac378c9f1f657
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-12-21 13:19:59 +0200

    Added configuration file. Done all TODO statements

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,7 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
 
     public static void main(String[] args) {
