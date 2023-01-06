9b6208e5518eea02c52b6f3fbfad36263eebda51
Uka0001 <ukraine0001@gmail.com>
2023-01-06 19:04:10 +0200

    fix gitignore

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can`t log user", e);
+            logger.error("Can`t log in user", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
