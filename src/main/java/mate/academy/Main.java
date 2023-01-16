a36f342ab82f6f51c88be6cdf9586c73c4489a19
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-16 13:54:48 +0200

    add solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,23 +1,30 @@
 package mate.academy;
 
 import mate.academy.exception.AuthenticationException;
+import mate.academy.model.Order;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final AuthenticationService authenticationService  = new AuthenticationServiceImpl();
+    private static final OrderService orderService = new OrderServiceImpl();
+
+
     public static void main(String[] args) {
-        AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Authentication was failed, reason: " + e);
             return;
         }
-        OrderService orderService = new OrderServiceImpl();
-        orderService.completeOrder(user.getUserId());
+        Order order = orderService.completeOrder(user.getUserId());
+        logger.info("User {} created and completed order with ID: {}. Program work without errors.", user.getLogin(), order.getOrderId());
     }
 }
