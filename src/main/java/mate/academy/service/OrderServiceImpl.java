b99cf18f543087adcd46e3b683c63a7598374b74
Richard <richarddkochut@gmail.com>
2023-01-17 08:40:55 +0200

    add console log

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("successful fetched data from DB. Params: userId = {}", userId);
+        logger.info("successful fetched data from DB. Params: userId = {}, products = {}",
+                userId,products);
         return products;
     }
 }
