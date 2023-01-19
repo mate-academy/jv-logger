35f3b2cb03359551f4868223041906288a5554be
Serhii Berdychevskyi <sirko120690@mail.com>
2023-01-19 14:46:14 +0200

    Added third versio jv-logger

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -12,7 +12,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String login, String password) throws AuthenticationException {
         logger.info("Method login was called with param: login = {}", login);
-        //TODO: add corresponding log message about method login was called
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
@@ -22,7 +21,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private User findByLogin(String login) {
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
     }
