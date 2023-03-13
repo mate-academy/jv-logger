48e43e01d4acf3a8b7fb3b53e6ebc2bec6a1c2e7
Nazar Zvarych <zvarych45@gmail.com>
2023-03-13 11:43:21 +0200

    I configure logger in this project

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Problem with user authentication: " + e);
+            logger.error("Problem with user authentication: ", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
