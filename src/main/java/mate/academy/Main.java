ad136979bded8f5ce6452f5bdcad394aaf2e3634
OlehSarapuk <olehsarapuk@gmail.com>
2023-03-20 16:15:29 +0200

    added fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private final static Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
