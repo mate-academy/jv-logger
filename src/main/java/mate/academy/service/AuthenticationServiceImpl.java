3b88f4051a923cbe56ce1891cc00cb8d51f33a6d
kromchk <kromchyk@yahoo.com>
2023-03-21 11:17:40 +0200

    realization of jv-logger <?>

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("findByLogin method was called. Login: {} <- ", login);
+        logger.info("Login method was called. Login: {} <- ", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password was incorrect");
