3e8d77457e1849cd987ac80800e28ae268b6e5f3
Mykyta Nikonets <nikita.nikonets@gmail.com>
2023-03-21 14:19:36 +0200

    added logger to project

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,15 +6,18 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(Main.class);
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user;
+        User user = new User("Bob", "1234");
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(user.getLogin(), user.getPassword());
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Authentication failed for login: " + user.getLogin());
             return;
         }
         OrderService orderService = new OrderServiceImpl();
