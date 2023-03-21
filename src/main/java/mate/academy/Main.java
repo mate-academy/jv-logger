06b33e3ae31ef1a43f5e84911e4c74d81aa76364
Skrudj <ssinilov@gmail.com>
2023-03-21 17:18:10 +0200

    configured logger, added it to the project, implemented toString method for Product class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,15 +6,22 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        String login = "bob";
+        String password = "1234";
+
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(login, password);
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Invalid attempt to login by {};", login);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
