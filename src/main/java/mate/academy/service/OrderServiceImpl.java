d3cb5127fbdc8331cded43aedc92568d8b673390
Artem Ponomarenko <artemponomar98@gmail.com>
2023-03-21 12:52:29 +0200

    Fixed checkstyle errors

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package mate.academy.service;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import mate.academy.model.Order;
 import mate.academy.model.Product;
 import org.apache.logging.log4j.LogManager;
