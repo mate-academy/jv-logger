7ca42f4223a87003ca1c4a04b257acb792c40e20
Ilko Good <ilko.engineering@gmail.com>
2023-03-21 10:57:11 +0200

    changed log level according to review

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("Method completeOrder performed successfully with userId {}", userId);
+        logger.info("Products were fetched from DB for userId {}", userId);
         return products;
     }
 }
