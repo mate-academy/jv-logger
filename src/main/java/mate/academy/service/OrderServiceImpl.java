0d52f2695cf1cfc968b78b61cc75a753ec0f9c1e
Oleksandr Krasnov <redman2775@gmail.com>
2023-03-21 12:39:30 +0200

    removed redundant comments and TODO rows

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,6 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        // TODO: add log message about method completeOrder was called
         logger.info("completeOrder method was called. Params: userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
@@ -23,15 +22,12 @@ public class OrderServiceImpl implements OrderService {
     }
 
     private List<Product> getAllProductsFromShoppingCart(Long userId) {
-        //logger.info("getAllProductsFromShoppingCart method was called. Params: userId = {}"
-        // , userId);
         // NOTE: In production ready code this method should fetch data from DB
         // For test purpose we simplify this method and return dummy data
         Product iphone = new Product("iPhone X", BigDecimal.valueOf(1199));
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        // TODO: add log message about successful fetched data from DB
         logger.info("data successfully fetched from DB. Params: userId = {}", userId);
         return products;
     }
