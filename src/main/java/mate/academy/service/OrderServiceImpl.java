ffd8cba93f4d2ae82c4c0e73c07b927dc3b87da2
Sushko Roman <formatesus@gmail.com>
2023-03-21 13:56:11 +0200

    changed level logger from debug to info

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("Data was successfully retrieved from the DB."
+        logger.info("Data was successfully retrieved from the DB."
                 + " User ID = {}, Quantity of products = {}", userId, products.size());
         return products;
     }
