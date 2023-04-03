5fd633f67f9d6b276ac0a9413cd3fc0d7d603fab
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-04-03 17:03:21 +0300

    fix issues with logger info out

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(Long userId) {
-        logger.info("completeOrder method was called. Params = {}", userId);
+        logger.info("completeOrder method was called. userId = {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
@@ -24,7 +24,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("successful fetched data from DataBase");
+        logger.info("successful fetched data from DataBase "
+                + "Products: {}", products.stream().iterator().next().getTitle());
         return products;
     }
 }
