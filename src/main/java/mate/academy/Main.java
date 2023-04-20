40907171af65032aa674e5a59ea6cb0b428d1cd0
iamchgi <imchgi1976@gmail.com>
2023-04-20 21:22:55 +0300

    Initial commit, log4j start configure

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,9 +6,14 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 
 public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
+        log.info("Did it again!");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
