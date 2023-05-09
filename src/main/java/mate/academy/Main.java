e3638b2ee27ad3f4dafbd94b641bdb50586d8510
Oles <a.vykhovanok@gmail.com>
2023-05-09 15:28:06 +0300

    fixed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            log.error(e.getMessage());
+            log.error("AuthenticationException - {}", e.getMessage());
             return;
         }
         OrderService orderService = new OrderServiceImpl();
