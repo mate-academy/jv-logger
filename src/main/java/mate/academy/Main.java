31bbefdda403e637aa14010f77c9df7cf04ae950
Tim <tymurprokhorov@gmail.com>
2023-05-22 22:03:54 +0300

    logger error message fixed, list of products added for logging

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can`t login user. Please check login or password", e);
+            logger.error("Authentication failed for user with login: {}", "bob", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
