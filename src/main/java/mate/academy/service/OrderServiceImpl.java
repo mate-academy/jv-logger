18653cd2136097473fe0973f5f418585e785f555
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-05-22 23:38:39 +0300

    realization logger

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 
 public class OrderServiceImpl implements OrderService {
     private static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
+
     @Override
     public Order completeOrder(Long userId) {
         logger.info("method completeOrder was called Params: userId = {}", userId);
@@ -27,7 +28,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Method completeOrder was called Params: userId = {} and successful fetched data from DB", userId);
+        logger.info("Method completeOrder was called Params: userId = "
+                + "{} and successful fetched data from DB", userId);
         return products;
     }
 }
