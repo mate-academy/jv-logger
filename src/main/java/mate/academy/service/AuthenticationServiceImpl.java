1d1f294a645360827334dfbeadc127267d04ec34
Tim <tymurprokhorov@gmail.com>
2023-05-23 14:11:21 +0300

    exception returned

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.service;
 
+import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,11 +9,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
 
     @Override
-    public User login(String login, String password) {
+    public User login(String login, String password) throws AuthenticationException {
         logger.info("login() was called. Params: login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             logger.error("Username or password are incorrect. Login: " + login);
+            throw new AuthenticationException("Username or password are incorrect");
         }
         return user;
     }
