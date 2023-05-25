c347d837e3a6746ed67da6db1ddafda8196b8209
Sergii Turchyn <turchyn13@gmail.com>
2023-05-25 21:30:17 +0300

    gitignore and exception were corrected

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -12,14 +12,14 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws AuthenticationException {
+    public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
             logger.error("Can't login.", e);
-            throw new AuthenticationException("Can't login.");
+            return;
         }
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
