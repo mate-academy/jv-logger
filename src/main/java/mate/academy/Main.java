01633efbfa6e063139a2722553a3f493a38ccf48
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-06-06 16:19:59 +0300

    add fixes after mentor review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can`t login", e);
+            logger.error("Can`t login. Invalid login or password", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
