ab18cf1d47ed6f476b6b4823bf7d1aa4439e1f0a
Володимир <vova.malyarchuk2@gmail.com>
2023-06-07 12:35:53 +0300

    Implemented task logger

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Login is incorrect");
+            logger.error("Username or password are incorrect", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
