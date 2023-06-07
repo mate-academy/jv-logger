d7f257a4e88333c98ba92188cb638e653ff03d8d
Vladyslav <vladkrivec3@gmail.com>
2023-06-07 14:25:29 +0300

    fixed problems

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Login is incorrect");
+            logger.error("Login is incorrect", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
