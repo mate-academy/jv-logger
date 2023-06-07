34d82830766d7964d4dd85daeaff81e49008fa0a
Oleksandr Balan <litecloud.nohax@gmail.com>
2023-06-07 12:47:49 +0300

    second version of task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,9 +6,14 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.debug("the program was executed.");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
