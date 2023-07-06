4a45bda0d0d2a0c3b0bdd78c25011c18d0707283
Yurii Ivanov <ivanovyura76@gmail.com>
2023-07-06 13:05:55 +0300

    Remove all TODO comments

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -11,7 +11,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
         logger.info("login method was called. Params: login={}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
