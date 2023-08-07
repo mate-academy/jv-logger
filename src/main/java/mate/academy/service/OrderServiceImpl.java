1416c679bee2158bde9cc6db8459a59ba0768c66
Влад <dakadofordisluv@gmail.com>
2023-08-07 18:53:44 +0300

    grammar fixed

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        LOGGER.info("successful fetched data from DB. Params: userId = {}", userId);
+        LOGGER.info("successfully fetched data from DB. Params: userId = {}", userId);
         return products;
     }
 }
