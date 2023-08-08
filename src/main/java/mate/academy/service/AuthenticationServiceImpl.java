682c82cff37804f8f85f3a9c57806b244a13c89c
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-08-08 23:20:17 +0300

    added logging operations in service methods

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,11 +2,16 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger LOGGER = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        LOGGER.info("'login' method was invoked "
+                + "with next arguments: user login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
@@ -16,7 +21,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private User findByLogin(String login) {
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
+        LOGGER.info("'findByLogin' method was invoked "
+                + "with next arguments: user login = {}", login);
         user.setUserId(2L);
         return user;
     }
