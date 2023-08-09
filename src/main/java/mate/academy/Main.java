374d56459149fae1bf4af2ef8a1fb7ee5c38b421
Oleksandr Savenets <savenvoin@gmail.com>
2023-08-09 17:27:48 +0300

    add empty line, change info level to debug

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -14,11 +14,12 @@ public class Main {
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user = null;
+        User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
             logger.error("Can't login", e);
+            return;
         }
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("The data from DB fetched successful");
+        logger.debug("The data from DB fetched successful");
         return products;
     }
 }
