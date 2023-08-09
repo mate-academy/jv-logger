38945c6aa104043b9fa5291e938595f8a5910997
Visqe <veetee@tutanota.com>
2023-08-09 17:35:28 +0300

    changed some logs levels

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Login failed" + e);
+            logger.error("Login failed", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
