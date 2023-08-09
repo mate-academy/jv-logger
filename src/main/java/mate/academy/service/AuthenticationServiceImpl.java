71b4b9be8626643f582180fdfba94e7211e596bb
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-08-09 16:38:02 +0300

    added more information to loggers

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.debug("login method was called");
+        logger.debug("login method was called by user:{}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
