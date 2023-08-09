b74d07809615d626e4b31376ff12e4064fc69b00
DaniilNepelev <kakashilvng@gmail.com>
2023-08-09 10:07:00 +0300

    fix minor mistackes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,7 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
@@ -21,7 +20,7 @@ public class Main {
         try {
             user = authenticationService.login(userName, userPassword);
         } catch (AuthenticationException e) {
-            logger.error("Can't login user with login: " + userName);
+            logger.error("Can't login user with login: " + userName, e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OrderServiceImpl implements OrderService {
-    private static final Logger logger = LogManager.getLogger(AuthenticationService.class);
+    private static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
 
     @Override
     public Order completeOrder(Long userId) {
