6d039d7d275ee76a186c88e2e83d3414023e9409
testSouth <southparaplan@gmail.com>
2023-09-16 10:39:40 +0300

    fixed mentors remarks

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
         logger.info("Login method was called. Params: login = {}", login);
 
         User user = findByLogin(login);
