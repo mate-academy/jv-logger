628c14620041056dfddfb3dedf02e3fc666a7273
Petro Smoliar <smp22pl@gmail.com>
2023-09-19 17:34:04 +0200

    1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,9 +6,14 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("log4j.configurationFile", "classpath:log4j2.xml");
+        Logger loger = LogManager.getLogger(Main.class);
+        loger.info("jlfnlfn");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
