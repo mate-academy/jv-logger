a5a2193c1cede49b0d07bc8fe21b139dcba227ce
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-09-20 17:24:10 +0300

    add text message in logger trace

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.trace(e);
+            logger.trace("User, couldn't authenticate", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
