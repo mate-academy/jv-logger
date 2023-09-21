0aedd6d043d8e5168c30604e0596da4c12a65a20
Parashchuk <Andry.parashchuk@gmail.com>
2023-09-21 09:22:14 +0200

    solved mistakes 1

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -11,7 +11,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("Login method was called, with login: {}", login);
+        logger.debug("Performing login for {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
