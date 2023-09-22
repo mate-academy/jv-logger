0b92db63460d3c400d2a4debedcb1d453e0820c9
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-09-22 11:54:32 +0300

    fixed comments

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't authenticate user, " + e);
+            logger.error("Can't authenticate user, ", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
