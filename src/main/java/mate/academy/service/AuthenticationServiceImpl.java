9a99f80664cbbe1cd5506c71eac2b1914ec7d520
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-09-22 13:07:35 +0300

    Changed to debud messages

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,7 +10,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("Processing your login: {}", login);
+        logger.debug("Processing input login: {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
