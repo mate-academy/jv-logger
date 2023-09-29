c00f88cbf0be6b0b4e6f442287a971da83565ef4
hp <volodya.gata@gmail.com>
2023-09-29 18:34:48 +0300

    fixed weird messages

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Error during using logger", e);
+            logger.error("Authentication was failed", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
