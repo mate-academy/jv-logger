42a839b79ace733b5cf597f87e64d5d1bd5a271f
evgenkolner <evgen.kolner@gmail.com>
2023-10-03 11:54:51 +0300

    add solution2

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,31 +1,13 @@
 package mate.academy.exception;
 
-import mate.academy.model.User;
-import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class AuthenticationException extends Throwable implements AuthenticationService {
+public class AuthenticationException extends Exception {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
 
     public AuthenticationException(String message) {
         super(message);
-    }
-
-    @Override
-    public User login(String login, String password) throws AuthenticationException {
-        logger.debug("Performing login for user {}", login);
-        User user = findByLogin(login);
-        if (!user.getPassword().equals(password)) {
-            throw new AuthenticationException("Username or password are incorrect");
-        }
-        return user;
-    }
-
-    private User findByLogin(String login) {
-        User user = new User(login,"1234");
-        user.setUserId(2L);
-        return user;
     }
 }
