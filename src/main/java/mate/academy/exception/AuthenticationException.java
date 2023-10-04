6096eb589dc71b71ca65fffbd1f86bde31354a57
evgenkolner <evgen.kolner@gmail.com>
2023-10-04 15:54:42 +0300

    add solution3

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,11 +1,6 @@
 package mate.academy.exception;
 
-import mate.academy.service.AuthenticationServiceImpl;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class AuthenticationException extends Exception {
-    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
 
     public AuthenticationException(String message) {
         super(message);
