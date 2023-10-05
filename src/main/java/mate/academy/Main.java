eed3a83d567f90b93c2658a752a5f9f923081a5a
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-10-05 15:26:03 +0300

    fix marks after review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.warn("Authentication error", e);
+            logger.error("Authentication error", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
