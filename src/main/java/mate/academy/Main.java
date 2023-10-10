63af4fbb6cf948e58c527cbf4a21db207576e7af
Danylo <kdanil2000@gmail.com>
2023-10-10 15:41:10 +0300

    Hide folder logs1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,4 +1,5 @@
 package mate.academy;
+
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -7,7 +8,6 @@ import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
