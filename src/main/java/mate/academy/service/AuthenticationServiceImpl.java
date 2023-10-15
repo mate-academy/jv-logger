b19df1a0ea0bfd34e1d71553919c83418c347039
Vlad Kucher <kuchervlad49@gmail.com>
2023-10-15 16:27:32 +0300

    mvn clean package done

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
         logger.debug("Method login was called. Params: login={}", login);
