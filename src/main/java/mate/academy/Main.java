1d9b319847b8e9b4cdb2dd21788da0e7034f544f
artificialLeo <horkovenko.k@gmail.com>
2023-11-01 11:47:51 +0200

    done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,18 +6,29 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
+            // Log successful authentication
+            logger.info("User {} has been successfully authenticated", user.getLogin());
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            // Log authentication failure
+            logger.error("Authentication failed: {}", e.getMessage());
             return;
         }
+
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
+
+        // Log completion of the order
+        logger.info("Order successfully completed for user with ID: {}", user.getUserId());
     }
 }
