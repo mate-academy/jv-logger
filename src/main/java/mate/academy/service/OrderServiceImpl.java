81d85f0b0b20a86c6ac082bb917b90a1c7b363bd
Hlib Bykovskyi <minexgleb@gmail.com>
2023-11-29 23:32:57 +0200

    Remove redundant dependency; add details to info logging OrderServiceImpl

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from DB was successfully fetched");
+        logger.info("Data from DB was successfully fetched, params: userId={}", userId);
         return products;
     }
 }
