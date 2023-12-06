4d3739c7416517ce2f2b66bc193bb131d7bb8343
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2023-12-06 17:40:48 +0200

    improved_solution2

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Login or password is wrong");
+            logger.error("Can not login", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
