04b84dcdf74fb5157363aa0be0986fd31ecfd835
a_shtuka <ashtuka@gmail.com>
2023-12-14 16:14:28 +0200

    Create DEFAULT_LOGIN and DEFAULT_PASSWORD variables. Add login param for log msg

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,14 +11,16 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final String DEFAULT_LOGIN = "bob";
+    private static final String DEFAULT_PASSWORD = "1234";
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(DEFAULT_LOGIN, DEFAULT_PASSWORD);
         } catch (AuthenticationException e) {
-            logger.error("Can't login", e);
+            logger.error("Can't login by login=" + DEFAULT_LOGIN, e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
