114d7b76f8ddba330e7bda5841db20eacf4578d3
Ivan <lavrinenkoivan51@gmail.com>
2023-12-15 17:24:27 +0100

    log4j was configured and log messages were added instead of TODO statements and e.printStackTrace

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -1,23 +1,33 @@
 package mate.academy.service;
 
+import mate.academy.Main;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
         //TODO: add corresponding log message about method login was called
+        logger.info("login was called. Params: login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
+            logger.error("Username or password are incorrect");
             throw new AuthenticationException("Username or password are incorrect");
         }
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.info("findByLogin was called. Params: login = {}", login);
         User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
+        logger.info("new user was created. Params: login = {}", login);
         user.setUserId(2L);
+        logger.info("user id was attributed");
         return user;
     }
 }
