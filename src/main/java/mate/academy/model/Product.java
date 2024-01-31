7ee44982b3a19045bacdb6980e7d576736423d4a
Bohdan <bohdan2704@gmail.com>
2024-02-01 00:45:10 +0200

    init commit

--- src/main/java/mate/academy/model/Product.java
+++ src/main/java/mate/academy/model/Product.java
@@ -1,32 +1,41 @@
 package mate.academy.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.math.BigDecimal;
 
 public class Product {
+    private static final Logger logger = LogManager.getLogger(Product.class);
     private String title;
     private BigDecimal price;
 
     public Product() {
+        logger.trace("Product constructor called");
     }
 
     public Product(String title, BigDecimal price) {
         this.title = title;
         this.price = price;
+        logger.trace("Product constructor called with title: {} and price: {}", title, price);
     }
 
     public String getTitle() {
+        logger.trace("getTitle method called");
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+        logger.trace("setTitle method called with title: {}", title);
     }
 
     public BigDecimal getPrice() {
+        logger.trace("getPrice method called");
         return price;
     }
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+        logger.trace("setPrice method called with price: {}", price);
     }
 }
--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,6 +1,10 @@
 package mate.academy.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class User {
+    private static final Logger logger = LogManager.getLogger(User.class);
     private Long userId;
     private String login;
     private String password;
@@ -8,29 +12,36 @@ public class User {
     public User(String login, String password) {
         this.login = login;
         this.password = password;
+        logger.trace("User constructor called with login: {}", login);
     }
 
     public Long getUserId() {
+        logger.trace("getUserId method called");
         return userId;
     }
 
     public void setUserId(Long userId) {
         this.userId = userId;
+        logger.trace("setUserId method called with userId: {}", userId);
     }
 
     public String getLogin() {
+        logger.trace("getLogin method called");
         return login;
     }
 
     public void setLogin(String login) {
         this.login = login;
+        logger.trace("setLogin method called with login: {}", login);
     }
 
     public String getPassword() {
+        logger.trace("getPassword method called");
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+        logger.trace("setPassword method called with password: {}", password);
     }
 }
