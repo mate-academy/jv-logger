2291a2be847d709621120742aade8bd31456807f
Yana Stepanova <yanna.stepanova@gmail.com>
2024-03-03 22:01:13 +0200

    added solution to the task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,15 +6,20 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOGGER.info("This is an 'info' level log message!");
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            LOGGER.error("This is an 'error' level log message!", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
