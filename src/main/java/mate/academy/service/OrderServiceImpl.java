c7d383f7252536037fc23fe025cc901ce289f9b6
makaroshyna <katya23225@gmail.com>
2024-03-22 16:47:16 +0200

    Change login level in order service.

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("Data was successfully fetched from DB. Using param UserId={}", userId);
+        logger.info("Data was successfully fetched from DB. Using param UserId={}", userId);
         return products;
     }
 }
