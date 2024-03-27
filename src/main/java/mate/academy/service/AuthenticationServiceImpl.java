aac9414a1a1a59bbea669c96a0a8bf7fe2aa2e32
John <pandastars535@gmail.com>
2024-03-27 17:52:58 +0200

    made MVN clean package

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
         User user = findByLogin(login);
