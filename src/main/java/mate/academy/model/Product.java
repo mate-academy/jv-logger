8bec0f0793fc36e4e31e39b01f188886576503d0
Pavlo Betsa <bigworld2303@gmail.com>
2024-03-27 15:53:50 +0200

    jv-logger V1.1 - remove toString and change info to debug

--- src/main/java/mate/academy/model/Product.java
+++ src/main/java/mate/academy/model/Product.java
@@ -29,11 +29,4 @@ public class Product {
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
-
-    @Override
-    public String toString() {
-        return "Product{"
-                + "title='" + title + '\''
-                + ", price=" + price + '}';
-    }
 }
