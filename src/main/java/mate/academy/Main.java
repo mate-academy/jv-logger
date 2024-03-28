dcfd718c8c572a3aa5b45ad049a1689dda22792f
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-03-28 20:25:59 +0200

    1) add logging

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,14 +6,20 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    public static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user = new User("bill", "qwerty");
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
+            logger.debug("AuthenticationException was catched with user: login={}",
+                    user.getLogin());
             throw new RuntimeException("The user: " + user.getLogin()
                     + "haven't been authenticated", e);
         }
