997cbe304b9a160e16e12f96d152d7c78157b53e
SviatoslavTataryn <www.t.svyatoslav55@gmail.com>
2024-04-25 00:38:22 +0200

    - add log4j dependency - add log4j2.xml - add check working application

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,22 +2,33 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        logger.info("Login method called for user: {}", login);
+
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
+            logger.error("Authentication failed for user: {}", login);
             throw new AuthenticationException("Username or password are incorrect");
         }
+
+        logger.info("User authenticated successfully: {}", login);
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.info("Attempting to find user by login: {}", login);
+
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
+
+        logger.info("User found: {}", login);
         return user;
     }
 }
