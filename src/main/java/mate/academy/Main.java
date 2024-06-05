a4aa094ebf99d06d4404efaf8fce2d58a560daea
Roman K <romazan07sq@gmail.com>
2024-06-05 21:37:15 +0300

    implement task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,18 +3,22 @@ package mate.academy;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
-import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
-import mate.academy.service.OrderServiceImpl;
+import mate.academy.service.impl.AuthenticationServiceImpl;
+import mate.academy.service.impl.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Can't authentication user with login=bob.", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
