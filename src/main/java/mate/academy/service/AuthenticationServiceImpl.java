eedffb36fea1e22b692b09628ae982ee7fc38452
Yarolsav <kiritok712@gmail.com>
2024-06-17 23:31:19 +0200

    add_tracking_method

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -10,20 +10,23 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.info("Login method was called. Params: login = {}", login);
+        logger.debug("login() - start. Params: login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             logger.error("Login failed for user '{}'. Incorrect username or password.", login);
             throw new AuthenticationException("Username or password are incorrect");
         }
         logger.info("User '{}' authenticated successfully.", login);
+        logger.debug("login() - end. User: {}", user);
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.debug("findByLogin() - start. Params: login = {}", login);
         User user = new User(login, "1234");
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
+        logger.debug("findByLogin() - end. User: {}", user);
         return user;
     }
 }
