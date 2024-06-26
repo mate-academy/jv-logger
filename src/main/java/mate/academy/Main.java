10e9c0e3acb7443e52349bc105affd9673ef34c8
Anton <panyutin15@gmail.com>
2024-06-26 20:05:19 +0300

    fixed solution after AI advices

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -14,11 +14,15 @@ public class Main {
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user;
+        User user = null;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login", e);
+            try {
+                logger.error("Can't login. Params: userId = {}", user.getUserId(), e);
+            } catch (NullPointerException ex) {
+                logger.error("Can't login, because user = null", ex);
+            }
             return;
         }
         OrderService orderService = new OrderServiceImpl();
