82d553d91852182d7ce0ee4701280052c845c8a6
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-07-08 11:28:15 -0400

    jv-logger task solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't authenticate user. Invalid login of password", e);
+            logger.error("Can't authenticate user. Invalid login or password", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
