ba41b2a5da51d222ed8bcc1e2f36995b56ea6427
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-07-10 15:12:34 -0400

    Updated solution following PR feedback

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.debug("The data successfully fetched from DB by userId - {}", userId);
+        logger.info("The data successfully fetched from DB by userId - {}", userId);
         return products;
     }
 }
