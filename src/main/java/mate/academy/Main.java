45e8b37da0eedab8d68c55d82b9dfe490957310e
Margaryta <marharytayarmoliuk@gmail.com>
2024-08-08 02:57:04 +0300

    Fixed problems

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Authentication failed for user 'bob': '{}'" + e.getMessage());
+            logger.error("Authentication failed for user 'bob': {}",e.getMessage());
             return;
         }
         OrderService orderService = new OrderServiceImpl();
