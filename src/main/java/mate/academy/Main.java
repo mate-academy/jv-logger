6c5942b57c8dc6fbc3f54fa56a4cce4888848667
Nikolai <colya.reznick@gmail.com>
2024-08-23 17:39:44 +0300

    Added details in log messages

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,8 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login", e);
+            logger.error("Can't login user with username: {}",
+                    "bob", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
