30cb8a187ab3f2712c55c3eb4f8d08ac081fc441
Oleh B <oleg.busygin@gmail.com>
2024-09-02 04:21:40 +0300

    jw-logger task done

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.error("Data from DB successfully fetched. Params: userId={}", userId);
+        logger.debug("Data from DB successfully fetched. Params: userId={}", userId);
         return products;
     }
 }
