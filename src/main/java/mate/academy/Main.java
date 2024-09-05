757aae2bde1e2603a9437bd92c702712979e7779
Dmytro Lapikov <it.lapikov@gmail.com>
2024-09-05 15:12:14 +0300

    task done, logger works with console and file

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,18 +6,28 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
             return;
         }
         OrderService orderService = new OrderServiceImpl();
+        orderService.completeOrder(user.getUserId());
+
+        try {
+            user = authenticationService.login("boben", "12345");
+        } catch (AuthenticationException e) {
+            return;
+        }
         orderService.completeOrder(user.getUserId());
     }
 }
--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,15 +2,23 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
         //TODO: add corresponding log message about method login was called
         User user = findByLogin(login);
+        logger.info("Login method was called for User with login: {}", login);
         if (!user.getPassword().equals(password)) {
-            throw new AuthenticationException("Username or password are incorrect");
+            logger.error("WRONG PASSWORD! "
+                    + "AuthenticationException throws for User with login: {} ", login);
+            throw new AuthenticationException("Username or password are incorrect.");
         }
+        logger.info("User with login: {} successfully logged in.", login);
         return user;
     }
 
