437fa10ae184cd166cfd28508c1545239fe85454
Oleksandr Prots <sashaproc2289@gmail.com>
2024-09-16 18:33:12 +0300

    jv-logger

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl .class);
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
