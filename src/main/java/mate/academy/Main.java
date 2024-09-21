a285987be4d8f47639c6378567f6b9a612a6b5d7
svit7 <svit7kucherenko@gmail.com>
2024-09-21 09:38:08 +0300

    My hometask Logger

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -14,12 +14,14 @@ public class Main {
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
-        User user = null;
+        User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
             logger.error("Can't login user with login: {}", "bob", e);
+            return;
         }
+
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
     }
