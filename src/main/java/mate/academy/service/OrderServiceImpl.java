526366223df26c233b4d50da1bb4eaa085c39676
flamehost <flamehost@syneforge.com>
2024-09-22 17:33:19 +0300

    display edits

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -26,7 +26,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Method: getAllProductsFromShoppingCart completed successfully. User id: {}, Products: {}", userId, products);
+        logger.info("Method: getAllProductsFromShoppingCart completed successfully."
+                        + " User id: {}, Products: {}", userId, products);
         return products;
     }
 }
