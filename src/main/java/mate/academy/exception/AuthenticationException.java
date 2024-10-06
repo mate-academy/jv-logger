12a5c5e4596eed670c1b3de67bb6e0db9ef58e65
Xoteki <xotekijke@gmail.com>
2024-10-06 06:19:27 +0300

    first solution

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,8 +1,21 @@
 package mate.academy.exception;
 
+import java.io.Serial;
+
 public class AuthenticationException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public AuthenticationException(String message) {
         super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(Throwable cause) {
+        super(cause);
     }
 }
