c0012b3a19d83e913cd3f4e4ff26f35d45dc1f0c
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-10-31 16:51:46 +0000

    refactor: changes made according to mentor's feedback

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -7,26 +7,27 @@ import mate.academy.model.Product;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class OrderServiceImpl implements OrderService {
+public class OrderServiceImpl {
     private static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
 
-    @Override
     public Order completeOrder(Long userId) {
         logger.info("Completing order for user with ID: {}", userId);
-        List<Product> products = getAllProductsFromShoppingCart(userId);
-        Order order = new Order(products, userId);
-        order.setOrderId(1L);
-        logger.info("Order completed with ID: {}", order.getOrderId());
-        return order;
-    }
 
-    private List<Product> getAllProductsFromShoppingCart(Long userId) {
-        logger.debug("Fetching products for user ID: {}", userId);
         Product iphone = new Product("iPhone X", BigDecimal.valueOf(1199));
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Successfully fetched products from shopping cart for user ID: {}", userId);
-        return products;
+
+        logger.info("Successfully fetched {} products from shopping cart for user ID: {}",
+                products.size(), userId);
+        Order order = new Order();
+        order.setOrderId(1L);
+        order.setProducts(products);
+
+        logger.info("Order completed with ID: {} for {} products with total value: ${}",
+                order.getOrderId(), products.size(),
+                products.stream().map(Product::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add));
+
+        return order;
     }
 }
