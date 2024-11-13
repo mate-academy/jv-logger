c707d4b1cb37a36f82aed1c90bc924e6038c7b4f
Alinka Lyn <alinka.lyn@ukr.net>
2024-11-13 19:47:23 +0200

    solved task

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -6,11 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+    private static final Logger logger= LogManager.getLogger(AuthenticationServiceImpl.class);
 
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        logger.debug("Method login called with parameters: login={}", login);
+       logger.info("Method login called with parameters: login={}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             logger.warn("Authentication failed for user {}", login);
