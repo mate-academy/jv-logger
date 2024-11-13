5978ecacf2fe80998c3e7622bcb84a8433190637
Alinka Lyn <alinka.lyn@ukr.net>
2024-11-13 19:18:11 +0200

    solved task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,23 +1,29 @@
 package mate.academy;
 
 import mate.academy.exception.AuthenticationException;
+import mate.academy.model.Order;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.error("Failed to authenticate user. Reason: {}", e.getMessage(), e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
-        orderService.completeOrder(user.getUserId());
+        Order order = orderService.completeOrder(user.getUserId());
+        logger.info("Order completed successfully. Order details: {}", order);
     }
 }
--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,22 +2,29 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        logger.debug("Method login called with parameters: login={}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
+            logger.warn("Authentication failed for user {}", login);
             throw new AuthenticationException("Username or password are incorrect");
         }
+        logger.info("User {} successfully logged in", login);
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.debug("Searching for user with login {}", login);
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
+        logger.debug("User with login {} found, ID: {}", login, user.getUserId());
         return user;
     }
 }
