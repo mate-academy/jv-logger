4c69f57518507661d38766bbaa58d8f842038f1e
Ihor <ihor.rome@gmail.com>
2024-11-16 21:54:58 +0200

    add solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,15 +6,20 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+    private static final String BOB = "bob";
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(BOB, "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            log.error("Authentication exception thrown for user with login: \"{}\"", BOB);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
