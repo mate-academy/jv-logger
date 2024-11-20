c063e9e0fb847422c74541176d697d3a7999475a
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-20 13:26:25 +0100

    add logs

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,7 @@
 package mate.academy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -8,13 +10,15 @@ import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
 
 public class Main {
+    private static final Logger logger = Logger.getGlobal();
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Error while creating user!", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
