c1b820840d1807142eab1687e4055e43af01cdf2
Anton Iliashenko <windsweptofficial@gmail.com>
2025-01-06 08:17:16 +0100

    fixed-1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -16,7 +16,7 @@ public class Main {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
         try {
-            user = authenticationService.login("bob", "4124235");
+            user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
             logger.error("Can't login", e);
             return;
