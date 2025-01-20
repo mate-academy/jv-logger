d3c9030551b52c246dc9c5bb5f267c092bbf0980
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2025-01-20 18:29:48 +0200

    fixed checkstyle

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -2,7 +2,6 @@ package mate.academy.service;
 
 import java.math.BigDecimal;
 import java.util.List;
-
 import mate.academy.model.Order;
 import mate.academy.model.Product;
 import org.apache.logging.log4j.LogManager;
@@ -29,8 +28,8 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Products from shoppingCart was successful fetched from DB, " +
-                "Params: userId={}, products={}", userId, products);
+        logger.info("Products from shoppingCart was successful fetched from DB, "
+                + "Params: userId={}, products={}", userId, products);
         return products;
     }
 }
