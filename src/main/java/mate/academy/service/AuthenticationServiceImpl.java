a0c424cb14aa4df24f55670a31ac9ae0fa43867f
Karol Gajda <karolgaj97@interia.pl>
2025-01-21 17:19:12 +0100

    first solution

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,13 +2,20 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
+        logger.info("Method login was called. Try to login User - {}",login);
         //TODO: add corresponding log message about method login was called
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
+            logger.error("ERROR - Username or password are incorrect");
             throw new AuthenticationException("Username or password are incorrect");
         }
         return user;
