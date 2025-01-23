6d8935496c3767606e791d67c2e4e8cd9fb335c8
Kate Kraska <kate2002.kk42@gmail.com>
2025-01-23 19:01:17 +0200

    TODO completed, log set up, blank lines added

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -24,5 +24,6 @@ public class Main {
         }
         OrderService orderService = new OrderServiceImpl();
         orderService.completeOrder(user.getUserId());
+        logger.info("Current working directory: {}", System.getProperty("user.dir"));
     }
 }
--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -8,12 +8,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OrderServiceImpl implements OrderService {
-    private static final Logger logger
-            = LogManager.getLogger(OrderServiceImpl.class);
+    private static final Logger
+            logger = LogManager.getLogger(OrderServiceImpl.class);
 
     @Override
     public Order completeOrder(Long userId) {
-        // TODO: add log message about method completeOrder was called
         logger.info("Method completeOrder was called for userId: {}", userId);
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
@@ -30,7 +29,6 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        // TODO: add log message about successful fetched data from DB
         logger.info("Method getAllProductsFromShoppingCart was called for userId: {}", userId);
         return products;
     }
