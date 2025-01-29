42ddca3a06c45d9d6f3adacba31a929ff5c3d00a
dantesworld <dantesworld2@gmail.com>
2025-01-29 12:48:59 +0200

    second attempt

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Exception occurred during authentication", e);
+            logger.error("Authentication failed for user. Reason: {}", e.getMessage());
             return;
         }
         OrderService orderService = new OrderServiceImpl();
