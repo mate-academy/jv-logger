e474e057daf7a009ff40a4e6dd1ed6e6ec749c27
DariaBakal <bakal.dasha01@gmail.com>
2025-02-13 19:03:14 +0200

    configure the logger and completed all TODO statements

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,18 +6,22 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
         User user;
         try {
             user = authenticationService.login("bob", "1234");
+            orderService.completeOrder(user.getUserId());
         } catch (AuthenticationException e) {
-            e.printStackTrace();
-            return;
+            logger.error("Can't login", e);
         }
-        OrderService orderService = new OrderServiceImpl();
-        orderService.completeOrder(user.getUserId());
     }
 }
