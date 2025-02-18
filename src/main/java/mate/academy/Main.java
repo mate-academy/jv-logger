a2446563f9d7cf473a6528c3376b256c97432a54
DariaBakal <bakal.dasha01@gmail.com>
2025-02-18 21:53:17 +0200

    configure the logger and completed all TODO statements

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -16,11 +16,13 @@ public class Main {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         OrderService orderService = new OrderServiceImpl();
         User user;
+        String login = "bob";
         try {
-            user = authenticationService.login("bob", "1234");
-            orderService.completeOrder(user.getUserId());
+            user = authenticationService.login(login, "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login", e);
+            logger.error("Can't login. Params: login={}",login,e);
+            return;
         }
+        orderService.completeOrder(user.getUserId());
     }
 }
