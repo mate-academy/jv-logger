8e0041604cc325717c1c21b598bd6667d41f4c2b
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-03-12 12:56:39 +0200

    Configured the logger and added logger methods

--- src/main/java/mate/academy/model/Product.java
+++ src/main/java/mate/academy/model/Product.java
@@ -29,4 +29,9 @@ public class Product {
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
