b10eb82201e170aa96abc488b9d2dbfcabb8a017
Olha <olha.oleshchuk@gmail.com>
2025-03-19 17:30:41 +0200

    done AI's remarks - second try

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -15,7 +15,7 @@ public class OrderServiceImpl implements OrderService {
         List<Product> products = getAllProductsFromShoppingCart(userId);
         Order order = new Order(products, userId);
         order.setOrderId(1L);
-        logger.info("Method completeOrder was called");
+        logger.info("Method completeOrder was called for userId={}", userId);
         return order;
     }
 
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Successful fetched data from DB");
+        logger.info("Successful fetched data from DB for userId={}", userId);
         return products;
     }
 }
