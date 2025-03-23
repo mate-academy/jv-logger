f312bb954b0188e495bf8b5239f3ac22331c19b5
Luchko Roman <romander09@gmail.com>
2025-03-23 12:43:06 +0200

    fix notice

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("successful retrieval of userId: {} data from the database ", userId);
+        logger.info("Returning hardcoded product list for userId: {}", userId);
         return products;
     }
 }
